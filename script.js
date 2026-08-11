26488875e772782039a1aa0a9ca9b93891ae0c47
Er. Divyanshu Sharma <divyanshusharmabtech1998@gmail.com>
2026-08-11 20:01:29 +0530

    Fix scriptURL string formatting in script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Google Apps Script URL
-var scriptURL = https://script.google.com/macros/s/AKfycbwjJ0wuyAxZx5kbJSxBCLTBghQjiTzCc3gAa_-SH1jNBLNF4zSB2-p_hypgQ8YhiBhf/exec;
+var scriptURL = 'https://script.google.com/macros/s/AKfycbwjJ0wuyAxZx5kbJSxBCLTBghQjiTzCc3gAa_-SH1jNBLNF4zSB2-p_hypgQ8YhiBhf/exec';
 
 // Form submission handler
 function handleSubmit(event) {
